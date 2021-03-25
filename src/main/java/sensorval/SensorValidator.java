7df044fe455404a331ea2d78ae2869afb1aee6e8
Sushmitha Rajendran <rajendran.sushmitha@in.bosch.com>
2021-03-25 18:49:40 +0530

    Changed the methods as per requirements

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -2,30 +2,38 @@ package sensorval;
 
 import java.util.List;
 
-public class SensorValidator 
-{
-    public static boolean _give_me_a_good_name(double value, double nextValue, double maxDelta) {
-        if(nextValue - value > maxDelta) {
-            return false;
-        }
-        return true;
-    }
-    public static boolean validateSOCreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.05)) {
-            return false;
-            }
-        }
-        return true;
-    }
-    public static boolean validateCurrentreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.1)) {
-            return false;
-            }
-        }
-        return true;
-    }
+public class SensorValidator {
+	private static boolean checkIfSensorExceedsMaxDeflectionForTwoConsecutiveReadings(double value, double nextValue,
+			double maxDelta) {
+		if (nextValue - value > maxDelta) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean validateSensorReadings(List<Double> values) {
+		if (validateInputValues(values)) {
+			int lastButOneIndex = getLastButOneIndex(values);
+			return checkIfTheSensorIsGoodOrNoisy(values, lastButOneIndex);
+		}
+		return false;
+	}
+
+	private static boolean validateInputValues(List<Double> values) {
+		return null != values && values.size() > 0;
+	}
+
+	private static int getLastButOneIndex(List<Double> values) {
+		return values.size() - 1;
+	}
+
+	private static boolean checkIfTheSensorIsGoodOrNoisy(List<Double> valuesToCheck, int lastButOneIndex) {
+		for (int i = 0; i < lastButOneIndex; i++) {
+			if (!checkIfSensorExceedsMaxDeflectionForTwoConsecutiveReadings(valuesToCheck.get(i),
+					valuesToCheck.get(i + 1), 0.05)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
--- src/test/java/sensorval/SensorValidatorTest.java
+++ src/test/java/sensorval/SensorValidatorTest.java
@@ -1,7 +1,6 @@
 package sensorval;
 
 import static org.junit.Assert.*;
-import jdk.nashorn.internal.AssertsEnabled;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,13 +14,27 @@ public class SensorValidatorTest
     {
         Double[] readings = {0.0, 0.01, 0.5, 0.51};
         List<Double> socs = Arrays.asList(readings);
-        assertFalse(SensorValidator.validateSOCreadings(socs));
+        assertFalse(SensorValidator.validateSensorReadings(socs));
     }
     @Test
     public void reportsErrorWhenCurrentjumps()
     {
         Double[] readings = {0.03, 0.03, 0.03, 0.33};
         List<Double> currents = Arrays.asList(readings);
-        assertFalse(SensorValidator.validateCurrentreadings(currents));
+        assertFalse(SensorValidator.validateSensorReadings(currents));
     }
+    
+    @Test
+    public void reportsErrorWhenThereIsNoInputForCurrent()
+    {
+        Double[] readings = {};
+        List<Double> currents = Arrays.asList(readings);
+        assertFalse(SensorValidator.validateSensorReadings(currents));
+    }
+    
+    @Test
+    public void reportsErrorWhenTheInputIsNullForCurrent() {
+        assertFalse(SensorValidator.validateSensorReadings(null));
+    }
+   
 }
